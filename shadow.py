bc18c116c5c493de6bb7f1407aacd1e6d716478a
Rodrigo <nobrega@gmail.com>
2017-08-21 16:39:18 +0800

    List all files from a directory
    # v0.1.02
    # for Issue #3

--- shadow.py
+++ shadow.py
@@ -5,8 +5,8 @@
 # Python application to create an HTML file
 # from all files in a directory
 #
-# v0.1.01
-# for Issue #1
+# v0.1.02
+# for Issue #3
 #
 # Rodrigo Nobrega
 # 20170821-
@@ -25,6 +25,7 @@ class ShProcess(object):
     """
     def __init__(self, dir):
         self.directory = dir
+        self.contents = os.listdir(self.directory)
 
     def __str__(self):
         return 'This is an object to write the contents of [{}] as an HTML file.'.format(self.directory)
@@ -38,6 +39,8 @@ def test():
     d = input('Enter directory: ')
     a = ShProcess(d)
     print(a)
+    print('Contents:\n')
+    [print(i) for i in a.contents]
 
 
 # main loop
